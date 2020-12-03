cf67af0bdaf7ad382b5cbdade525205a3c812d68
dera- <alchemy.and.fairy@gmail.com>
2020-12-03 08:53:31 +0000

    tabun dame

--- generateReleaseNote/index.js
+++ generateReleaseNote/index.js
@@ -1,34 +1,19 @@
 const core = require("@actions/core");
 const fs = require("fs");
 const path = require("path");
+const generateReleaseNote = require("./generateReleaseNote");
 
 try {
-	const repositoryPath = core.getInput("repositoryPath");
+	const repositoryPath = core.getInput("repository_path");
 	const packageJson = require(path.join(repositoryPath, "package.json"));
 	const version = packageJson["version"];
-	console.log("version", version);
 	core.setOutput("version", "v" + version);
 	let body = "";
 	const changelogPath = path.join(repositoryPath, "CHANGELOG.md");
 	if (fs.existsSync(changelogPath)) {
 		const changelog = fs.readFileSync(changelogPath).toString();
-		const changelogArray = changelog.split("\n");
-		let matchCount = 0;
-		const regex = /## (\d+\.\d+\..+)/;
-		for (let i = 0; i < changelogArray.length; i++) {
-			const match = changelogArray[i].match(regex);
-			if (match) {
-				if (matchCount > 0) {
-					break;
-				} else if (match[1] === version) {
-					matchCount++;
-				}
-			} else if (matchCount > 0) {
-				body += changelogArray[i] + "\n";
-			}
-		}
+		body = generateReleaseNote(changelog, version);
 	}
-	console.log("body", body);
 	core.setOutput("body", body);
 } catch (error) {
 	core.setFailed(error.message);
