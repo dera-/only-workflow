0b0aac77a15a636ae48f5527a8242e3a1492ba1f
dera- <alchemy.and.fairy@gmail.com>
2021-01-20 16:51:20 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- mergeRenovatePullRequest/index.js
+++ mergeRenovatePullRequest/index.js
@@ -3,7 +3,8 @@ const github = require("@actions/github");
 
 try {
 	const repositoryName = core.getInput("repository_name");
-	const octokit = github.getOctokit(myToken);
+	const githubToken = core.getInput("github_token");
+	const octokit = github.getOctokit(githubToken);
 	octokit.pulls.list({
         owner: 'dera-',
         repo: repositoryName,
